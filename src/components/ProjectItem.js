d5e8b6d5695714f5ca125fc5f8e11e2bdd254e88
Alison Melendez <alisonmelendez132@gmail.com>
2022-02-02 10:52:32 -0500

    commit tree 50f00179cb5440c1e0f5931c2bb93d6d79ef0c41

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+  const sepProj = technologies.map((tech)=>{
+    // console.log(tech)
+    return <span key={tech}> {tech} </span> 
+    //where is the key passed in and how since there is no unique ID
+    
+  })
+
+  
+
   return (
+
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+         {sepProj}
       </div>
     </div>
   );
